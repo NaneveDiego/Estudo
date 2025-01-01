0b76ec4dda1af04e557d2152ca0b26ea575a2d2d
NaneveDiego <nanevediego@gmail.com>
2025-01-01 16:36:54 -0300

    Comentarios/algoritmos/diagramação

---
+++ Basico.py
@@ -0,0 +1,15 @@
+"""
+ Comentários -> # (linha) e """ """ (bloco)
+ 
+ Algoritmo -> é um processo de resolução de um problema, ou seja, 
+ as regras ou expressoes usadas para obtenção de um resultado ou solução
+
+ Diagramação -> 
+
+    1) Objetivo do sistema
+    2) entradas e saidas
+    3) processamento e condições
+
+
+
+ """
