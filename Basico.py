a34411cdd74964cae281b2de66edd547710c483a
NaneveDiego <nanevediego@gmail.com>
2025-01-02 16:54:07 -0300

    Basico 1.0

--- Basico.py
+++ Basico.py
@@ -10,6 +10,22 @@
     2) entradas e saidas
     3) processamento e condições
 
+ Pseudocódigo ou português estruturado
+ Descrição generica para uma linguagem de projeto de programação
+
+
+ Algoritmo "media"
+
+   declaração de variaveis
+   entradas do usuário
+   processamento de dados
+   saida do sistema
+
+fim do algoritmo   
+   
+ 
+ Python será utilizado para resolução dos problemas
+
 
 
  """
